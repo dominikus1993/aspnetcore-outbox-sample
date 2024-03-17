c51a860b6fc82541c2bc8b0f0714a28e5d290104
Dominik.Kotecki <dominikus1910@gmail.com>
2024-03-17 16:19:59 +0100

    add interceptor

---
+++ src/Sample.Api/Infrastructure/EfCore/OutboxInterceptor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Sample.Api.Core.Model;
+using Sample.Api.Infrastructure.Repositories;
+
+namespace Sample.Api.Infrastructure.EfCore;
+
+public sealed class OutboxInterceptor : SaveChangesInterceptor
+{
+    private readonly OutBoxEventCreator _creator;
+    private readonly TimeProvider _timeProvider;
+
+    public OutboxInterceptor(OutBoxEventCreator creator, TimeProvider timeProvider)
+    {
+        _creator = creator;
+        _timeProvider = timeProvider;
+    }
+
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result,
+        CancellationToken cancellationToken = default)
+    {
+        if (eventData.Context is null)
+        {
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+        
+        var order = eventData.Context.ChangeTracker.Entries<Order>().Select(e => e.Entity).FirstOrDefault();
+        if (order is null)
+        {
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        var outBox = _creator.Create(new OrderSaved(order, _timeProvider));
+        
+        eventData.Context.Add(outBox);
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+}
